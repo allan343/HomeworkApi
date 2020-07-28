18a10d50ef76a5f67ee164820eb0efc82a017a54
Allan Cheng <allanhfcheng@gmail.com>
2020-07-27 20:06:30 -0700

    tests

--- test/HomeworkService.spec.js
+++ test/HomeworkService.spec.js
@@ -73,7 +73,7 @@ describe(`Show List Service object`, function () {
         .then(allItems => {
           // copy the test items array without the removed item
           const expected = testItems
-            .filter(homework => homeworkid !== homeworkId);
+            .filter(homework => homework.homeworkid !== homeworkId);
           expect(allItems).to.eql(expected);
         })
     })
