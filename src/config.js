6f707df0001b62d4c4ca4ea5d4617525fade3a98
Allan Cheng <allanhfcheng@gmail.com>
2020-07-31 21:56:28 -0700

    will this work?

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://studentUser:mypassword@localhost/school-app',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://studentUser:mypassword@localhost/school-apptest'
-  
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://studentUser:mypassword@localhost/school-apptest',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN
 }
