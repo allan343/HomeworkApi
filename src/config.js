83aabaa4683e7d59ece6672dbd1dee6f2ddbe7c1
Allan Cheng <allanhfcheng@gmail.com>
2021-04-01 18:46:18 -0700

    stuff to fix

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://studentUser:mypassword@localhost/school-app',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://studentUser:mypassword@localhost/school-apptest',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://studentUser1:mypassword1@localhost/schoolapp1',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://studentUser1:mypassword1@localhost/schoolapptest1',
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN
 }
